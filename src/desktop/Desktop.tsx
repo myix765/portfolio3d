0939a67ff012b58a46e852fbeba030811e057535
myix765 <myix765@gmail.com>
2026-03-05 15:58:09 -0500

    use react-rnd for draggable and resizable window, styled window

--- src/desktop/Desktop.tsx
+++ src/desktop/Desktop.tsx
@@ -7,7 +7,7 @@ const Desktop = () => {
     <div className='w-480 h-273 bg-[url(/wallpapers/monet-poppy-fields-near-argenteuil.jpg)] bg-cover rounded-sm relative overflow-hidden'>
       <MenuBar />
       <Dock />
-      <Window initX={200} initY={200}>
+      <Window initX={200} initY={200} header='header'>
         hello
       </Window>
     </div>
--- src/desktop/components/Window.tsx
+++ src/desktop/components/Window.tsx
@@ -1,16 +1,38 @@
 import type { ReactNode } from 'react';
-import useDraggable from '../hooks/useDraggable';
+import { Rnd } from 'react-rnd';
 
-const Window = ({ children, initX, initY }: { children: ReactNode; initX: number; initY: number }) => {
-  const { pos, handleRef, handleProps } = useDraggable(initX, initY);
+const Window = ({
+  children,
+  initX,
+  initY,
+  header,
+}: {
+  children: ReactNode;
+  initX: number;
+  initY: number;
+  header: string;
+}) => {
+  const padding = 'p-4';
 
   return (
-    <div className='absolute w-4xl h-160 bg-amber-300 rounded-xl' style={{ left: pos.x, top: pos.y }}>
-      <div ref={handleRef} {...handleProps} className='header w-full bg-amber-700'>
-        header
+    <Rnd
+      dragHandleClassName='header'
+      default={{ x: initX, y: initY, width: 896, height: 640 }}
+      minWidth={'12%'}
+      minHeight={'12%'}
+      className='bg-white rounded-xl overflow-clip shadow-[0_36px_100px_0_rgba(0,0,0,0.4),0_0_3px_0_rgba(0,0,0,0.55)]'
+    >
+      <div className={`header w-full bg-neutral-300 ${padding} h-9 flex items-center`}>
+        <div className='flex flex-1 gap-2 items-center'>
+          <div id='close' className='bg-red-500 border border-red-700 w-3.5 h-3.5 rounded-full'></div>
+          <div id='minimize' className='bg-yellow-500 border border-yellow-600 w-3.5 h-3.5 rounded-full'></div>
+          <div id='fullscreen' className='bg-green-500 border border-green-700 w-3.5 h-3.5 rounded-full'></div>
+        </div>
+        <h2>{header}</h2>
+        <div className='flex-1'></div> {/* keep header centered */}
       </div>
-      {children}
-    </div>
+      <div className={`${padding}`}>{children}</div>
+    </Rnd>
   );
 };
 
