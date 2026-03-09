a5837d9b86c0054329025d3bcaef40387a3487fb
myix765 <myix765@gmail.com>
2026-03-09 02:19:30 -0400

    profile app responsiveness

--- src/desktop/apps/ProfileApp.tsx
+++ src/desktop/apps/ProfileApp.tsx
@@ -3,39 +3,41 @@ import AppSection from '../components/windows/AppSection';
 
 const ProfileApp = () => {
   return (
-    <div className='w-full h-full grid grid-cols-5 grid-rows-[repeat(3,minmax(min-content,1fr))] gap-[0.8vw] p-4'>
-      <AppSection colSpan={3}>
-        <img src={ProfileIconNoHi} alt='' className='rounded-xl max-h-[36vh] max-w-[36vh]' />
-        <div className='flex flex-col gap-y-4'>
-          <div>
-            <h1>Megan Yi</h1>
-            <p className='italic'>currently soon to be graduate</p>
+    <div className='w-full h-full @container'>
+      <div className='grid grid-cols-1 @3xl:grid-cols-5 grid-rows-[repeat(3,minmax(min-content,1fr))] gap-[0.8vw] p-4'>
+        <AppSection colSpan={3}>
+          <img src={ProfileIconNoHi} alt='' className='rounded-xl max-h-[36vh] max-w-[36vh]' />
+          <div className='flex flex-col gap-y-4'>
+            <div>
+              <h1>Megan Yi</h1>
+              <p className='italic'>currently soon to be graduate</p>
+            </div>
+            <div>
+              <h3>Bio:</h3>
+              <p>
+                Hi, I'm a software engineer mainly experienced in fullstack, but also interested in other fields like
+                computer vision! Outside of coding I also enjoy drawing, woodcarving, and photography.
+              </p>
+            </div>
+            <div>
+              <h3>Education</h3>
+              <p>Tufts University &apos;26, B.S.</p>
+            </div>
           </div>
-          <div>
-            <h3>Bio:</h3>
-            <p>
-              Hi, I'm a software engineer mainly experienced in fullstack, but also interested in other fields like
-              computer vision! Outside of coding I also enjoy drawing, woodcarving, and photography.
-            </p>
-          </div>
-          <div>
-            <h3>Education</h3>
-            <p>Tufts University &apos;26, B.S.</p>
-          </div>
-        </div>
-      </AppSection>
-      <AppSection colSpan={2} rowSpan={3}>
-        <h2>Experience</h2>
-      </AppSection>
-      <AppSection colSpan={3}>
-        <h2>Skills</h2>
-      </AppSection>
-      <AppSection>
-        <h2>Socials</h2>
-      </AppSection>
-      <AppSection colSpan={2}>
-        <h2>GitHub</h2>
-      </AppSection>
+        </AppSection>
+        <AppSection colSpan={2} rowSpan={3}>
+          <h2>Experience</h2>
+        </AppSection>
+        <AppSection colSpan={3}>
+          <h2>Skills</h2>
+        </AppSection>
+        <AppSection>
+          <h2>Socials</h2>
+        </AppSection>
+        <AppSection colSpan={2}>
+          <h2>GitHub</h2>
+        </AppSection>
+      </div>
     </div>
   );
 };
--- src/desktop/components/windows/AppSection.tsx
+++ src/desktop/components/windows/AppSection.tsx
@@ -8,11 +8,15 @@ interface AppSectionProps {
 
 const AppSection = ({ children, colSpan = 1, rowSpan = 1 }: AppSectionProps) => {
   const isVert = rowSpan > colSpan;
+  const colClass =
+    { 1: 'col-span-1', 2: 'col-span-2', 3: 'col-span-3', 4: 'col-span-4', 5: 'col-span-5' }[colSpan] ?? 'col-span-1';
+  const rowClass = { 1: 'row-span-1', 2: 'row-span-2', 3: 'row-span-3' }[rowSpan] ?? 'row-span-1';
   return (
     <section
-      style={{ gridColumn: `span ${colSpan}`, gridRow: `span ${rowSpan}` }}
       className={`border border-neutral-300 rounded-xl p-[1vw] min-h-0
-                  flex ${isVert && 'flex-col'} gap-[1vw]`}
+              flex ${isVert ? 'flex-col' : 'flex-row'} @max-3xl:flex-col gap-[1vw]
+              ${colClass} ${rowClass}
+              @max-3xl:col-span-full @max-3xl:row-span-1`}
     >
       {children}
     </section>
