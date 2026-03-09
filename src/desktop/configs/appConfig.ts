b431e9441b7c6f1f2f4582686e0604ca1aa74c5a
myix765 <myix765@gmail.com>
2026-03-09 17:20:25 -0400

    hide portfolio app for now

--- src/desktop/configs/appConfig.ts
+++ src/desktop/configs/appConfig.ts
@@ -1,8 +1,8 @@
 import type { ComponentType } from 'react';
 import profileIcon from '../assets/apps/profile_icon.png';
-import portfolioIcon from '../assets/apps/portfolio_icon.png';
+// import portfolioIcon from '../assets/apps/portfolio_icon.png';
 import ProfileApp from '../apps/ProfileApp';
-import PortfolioApp from '../apps/PortfolioApp';
+// import PortfolioApp from '../apps/PortfolioApp';
 
 export type AppIds = 'profile' | 'portfolio';
 
@@ -26,13 +26,13 @@ export const appConfig: AppConfig[] = [
     height: 720,
     defaultOpen: true,
   },
-  {
-    id: 'portfolio',
-    name: 'Portfolio',
-    icon: portfolioIcon,
-    component: PortfolioApp,
-    width: 1080,
-    height: 720,
-    defaultOpen: false,
-  },
+  // {
+  //   id: 'portfolio',
+  //   name: 'Portfolio',
+  //   icon: portfolioIcon,
+  //   component: PortfolioApp,
+  //   width: 1080,
+  //   height: 720,
+  //   defaultOpen: false,
+  // },
 ];
