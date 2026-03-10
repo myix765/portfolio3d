3b0a2169b5529f447f427ae8dab0e73b880fb767
myix765 <myix765@gmail.com>
2026-03-10 01:12:21 -0400

    add jumbohack project

---
+++ src/desktop/configs/appIcons.ts
@@ -0,0 +1,5 @@
+import profileIcon from '../assets/apps/profile_icon.png';
+import dndCatIcon from '../assets/apps/dndcat_app_icon.png';
+import jumboHackIcon from '../assets/apps/jumbohack_icon_yellow.svg';
+
+export { profileIcon, dndCatIcon, jumboHackIcon };
