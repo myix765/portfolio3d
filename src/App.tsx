cdfbd908be9d3cb2c0ae02dae6a49d039d0221cf
myix765 <myix765@gmail.com>
2026-03-24 12:18:34 -0400

    prevent pinch zooming in browsers

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,20 @@ import CameraSystem from './3d/components/camera/CameraSystem';
 import { freeLookOrigin } from './3d/components/configs/cameraConfigs';
 import { keyMap } from './3d/configs/interactConfig';
 import EscapeText from './3d/components/objects/EscapeText';
+import { useEffect } from 'react';
 // import { OrbitControls } from '@react-three/drei';
 
 const App = () => {
+  useEffect(() => {
+    window.addEventListener(
+      'wheel',
+      event => {
+        if (event.ctrlKey) event.preventDefault();
+      },
+      { passive: false },
+    );
+  }, []);
+
   return (
     <KeyboardControls map={keyMap}>
       <Canvas shadows camera={freeLookOrigin} className='z-0'>
